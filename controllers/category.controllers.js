378d32bfdf0d76043097328cc2772f36b1753208
SaileshNaganath <saileshnaganath@gmail.com>
2023-05-14 07:13:30 +0530

    new

---
+++ controllers/category.controllers.js
@@ -0,0 +1,129 @@
+/**
+ * This file contains the controller logic for the category resource.
+ * Every time a CRUD request come for the category,methods defined in this Controller file wil be executed
+ */
+const { category } = require("../models")
+const db = require("../models");
+const Category = db.category;
+
+/**
+ * POST:Create and save a new category
+ */
+
+exports.create =(req,res)=>{
+    /**
+     * Validation of request body
+     */
+    if(!req.body.name){
+        res.status(400).send({
+            message:"Name of the category can't be empty"
+        })
+        return;
+    }
+}
+
+/**
+ * Creation of the category object to be stored in db
+ */
+
+const category={
+    name:req.body.name,
+    description:req.body.description
+};
+
+Category.create(category)
+.then(category=>{
+    console.log(`Category Name: ${category.name} got inserted `)
+    res.status(201).send(category);
+})
+.catch(err=>{
+    console.log(`Issue in inserting category name: ${category.name} `)
+    console.log(`Error Message: ${err.message}`)
+    res.status(500).send({
+        message:"Some internal error while storing the category"
+    })
+})
+
+/**
+ * GET:Get the list of all categories
+ */
+
+
+exports.findAll =(req,res)=>{
+    let categoryName = req.query.name;
+    let promise;
+    if(categoryName){
+        promise=Category.finAll({
+            where:{
+                name:categoryName
+            }
+        });
+    }else{
+        promise=Category.findAll();
+    }
+
+
+promise
+.then(categories=>{
+    res.status(200).send(categories);
+})
+.catch(err=>{
+    res.status(500).send({
+        message:"Some internal error while fetching the categories"
+    });
+})
+}
+
+/**
+ * GET:Get teh category based on id
+ */
+
+exports.findOne=(req,res)=>{
+    const categoryID = req.params.id;
+
+    Category.findByPk(categoryID)
+    .then(category=>{
+        res.status(200).send(category);
+    })
+    .catch(err=>{
+        res.send(500).send({
+            message:"Some internal error while Fetching category based on ID"
+        })
+    })
+}
+
+/**
+ * Update the existing category
+ */
+
+exports.update=(req,res)=>{
+    const category={
+        name:req.body.name,
+        description:req.body.description
+    };
+
+    const categoryID =req.params.id;
+
+    Category.update(category)
+    .then (updateCategory=>{
+        //Where the updation happened successfully
+        //You need to send the updated row to the table.
+        //But while fetching that row and sending it to user there can be error.
+
+        Category.findByPk(categoryID)
+        .then(category=>{
+            res.status(200).send(category);
+        })
+        .catch(err=>{
+            res.status(500).send({
+                message:"Some Internal Error wHile Fetching the request"
+            })
+        })
+    })
+    .catch(err=>{
+        //where the update task failed
+        res.status(500).send({
+            message:"Some Internal Error wHile Updating the request"
+        })
+    })
+}
---
+++ routes/category.routes.js
@@ -0,0 +1,4 @@
+
+
+
+app.post("/ecom/api/v1/categories")
