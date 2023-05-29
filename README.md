552894ce3212247d43ea1c4a03dab4cb98a97244
SaileshNaganath <saileshnaganath@gmail.com>
2023-05-29 10:18:16 +0530

    m

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is Basic Ecommerce app backend project developed in node.js, express.js, mysql,jest
 
 the main dependecies are Node, Express, Sequelize, Mysql2, body-parser
+
+
+Routes->Controllers->Models->Database 
--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,16 @@
 module.exports={
+   development:{
     HOST:"127.0.0.1",
     USER:"root",
     PASSWORD:"hello123",
     DB:"ecom_db",
     dialect:"mysql" //RDBMS being used
+   },
+   production:{
+    HOST: "sql12.freemysqlhosting.net",
+    USER: "sql12622109",
+    PASSWORD: "WVHQwHW8ri",
+    DB:"sql12622109",
+    dialect:"mysql"
+   }
 }
